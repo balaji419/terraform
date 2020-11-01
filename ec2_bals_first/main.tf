e1fbe8c71166fe50d1040cfd236379c36032084e
Balaji Bava Srikumar <balaji.nandakumar419@gmail.com>
2020-11-01 07:08:20 +0530

    initial commit

---
+++ ec2_bals_first/main.tf
@@ -0,0 +1,11 @@
+# this is an place where you provide the terraform aws plugin version
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "3.12.0"
+    }
+  }
+
+ version=" >= 0.12.0"
+}
