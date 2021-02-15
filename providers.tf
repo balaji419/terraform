7882c7aabf12161db9c44f417b51cb06c0598804
balaji419 <33886990+balaji419@users.noreply.github.com>
2021-02-15 19:24:20 +0530

    Create providers.tf

---
+++ providers.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+  backend "s3" {
+    bucket = "terraformbacked"
+    key    = "terraformexample"
+    region = "us-east-2"
+  }
+}
+
+provider "aws" {
+  region = "us-east-2"
+}
